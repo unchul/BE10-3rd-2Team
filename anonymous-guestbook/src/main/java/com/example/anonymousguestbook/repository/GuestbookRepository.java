785ff2f39dbece04e786131b24f69a83bc34980b
GoldenDuck <unchul87@gmail.com>
2025-07-18 10:45:43 +0900

    feat:[임운철]백엔드 10기 3th-2team MSA프로젝트

--- anonymous-guestbook/src/main/java/com/example/anonymousguestbook/repository/GuestbookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.anonymousguestbook.repository;
-
-import com.example.anonymousguestbook.entity.Guestbook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
-}
